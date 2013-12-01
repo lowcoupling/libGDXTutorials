f027881c8573407cb05094d934ab59fc70fa29b0
lowcoupling <mdpmproject@gmail.com>
2013-12-01 09:27:12 +0100

    added tiledMap2 project with input from keyboard

---
+++ tiledMap1/testGdxGame/src/lowcoupling/tetGdx/TestInputProcessor.java
@@ -0,0 +1,78 @@
+package lowcoupling.tetGdx;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
+public class TestInputProcessor implements InputProcessor {
+	
+     	   private OrthographicCamera camera;
+
+		public TestInputProcessor(OrthographicCamera cam) {
+		// TODO Auto-generated constructor stub
+     		   this.camera=cam;
+	}
+
+		@Override
+		   public boolean keyDown (int keycode) {
+			int yoffset=0;
+			int xoffset=0;
+			if(keycode==19){
+				yoffset=20;
+			}else if(keycode==20){
+				yoffset=-20;
+			}else if (keycode==21){
+				xoffset=-20;
+			}else if (keycode==22){
+				xoffset=20;
+			}
+			camera.position.y+=yoffset;
+			camera.position.x+=xoffset;
+			
+			camera.viewportHeight = Gdx.graphics.getHeight();;
+			camera.viewportWidth = Gdx.graphics.getWidth();;
+			camera.update();
+		      return false;
+		   }
+
+		   @Override
+		   public boolean keyUp (int keycode) {
+		      return false;
+		   }
+
+		   @Override
+		   public boolean keyTyped (char character) {
+		      return false;
+		   }
+
+		   @Override
+		   public boolean touchDown (int x, int y, int pointer, int button) {
+		      return false;
+		   }
+
+		   @Override
+		   public boolean touchUp (int x, int y, int pointer, int button) {
+		      return false;
+		   }
+
+		   @Override
+		   public boolean touchDragged (int x, int y, int pointer) {
+		      return false;
+		   }
+
+		   public boolean touchMoved (int x, int y) {
+		      return false;
+		   }
+
+		   @Override
+		   public boolean scrolled (int amount) {
+		      return false;
+		   }
+
+		@Override
+		public boolean mouseMoved(int screenX, int screenY) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+}
---
+++ tiledMap2/testGdxGame/src/lowcoupling/testGdx/MyGdxGame.java
@@ -0,0 +1,61 @@
+package lowcoupling.testGdx;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
+public class MyGdxGame implements ApplicationListener {
+	private OrthographicCamera camera;
+	private OrthogonalTiledMapRenderer mapRenderer;
+    private TiledMap map;	     
+	@Override
+	public void create() {		
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
+		Texture.setEnforcePotImages(false);
+	     map = new TmxMapLoader().load("data/desert.tmx");
+	     mapRenderer = new OrthogonalTiledMapRenderer(map);
+		 camera = new OrthographicCamera();
+		TestInputProcessor ip = new TestInputProcessor(camera);
+		Gdx.input.setInputProcessor(ip);;
+		camera.position.y=700;
+		camera.position.x=300;
+	}
+	@Override
+	public void dispose() {
+		mapRenderer.dispose();
+		map.dispose();
+	}
+	@Override
+	public void render() {		
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		mapRenderer.setView(camera);
+		mapRenderer.render();
+	}
+	@Override
+	public void resize(int width, int height) {
+		camera.viewportHeight = height;
+		camera.viewportWidth = width;
+		
+		camera.update();
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+}
